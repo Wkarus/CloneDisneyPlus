1844e9b101a22147a31c267c79aab95f3025ddf3
Icaro Souza <icarololo13@gmail.com>
2024-01-16 15:55:15 -0300

    criando a sessão de planos

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click',function(botao) {
+        buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-            escondeTodasAsAbas();
+            escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
@@ -14,17 +14,18 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     })
 
-        function removeBotaoAtivo(){
-            const buttons = document.querySelectorAll('[data-tab-button');
+    function removeBotaoAtivo() {
+            const buttons = document.querySelectorAll('[data-tab-button]');
+            
             for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
             }
         }
 
-    function escondeTodasAsAbas(){
-            const tabsContainer = document.querySelectorAll('[data-tab-id');
+    function escondeTodasAbas() {
+            const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
-            for(let i = 0; i < tabsContainer.length; i++){
-                tabsContainer[i].classList.remove('shows_list--is-active');
+            for (let i = 0; i < tabsContainer.length; i++){
+        tabsContainer[i].classList.remove('shows__list--is-active');
             }
     }
